bbc44a97ddeb6130e13fd12e9af01c8323c89325
rainer <rainer.telesko@fhnw.ch>
2021-02-11 09:18:42 +0100

    Update version

--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -11,7 +11,7 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
